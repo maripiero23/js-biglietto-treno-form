0807a265d0b02e4c7c701e44619b1994ec270f2c
maripiero23 <mari.pieropan@gmail.com>
2022-11-02 08:43:19 +0100

    correzione col per responsive

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 const buttonGenera = document.getElementById(btnGenera)
 const buttonAnnulla = document.getElementById(btnAnnulla)
+
+buttonGener.addEventListener( "click", function (){
+    console.log( "Hai cliccato il pulsante" );
+}
