5e5238925839c96741e99b16e605815de6ca5b80
maripiero23 <mari.pieropan@gmail.com>
2022-11-02 09:26:21 +0100

    js doing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-const buttonGenera = document.getElementById(btnGenera)
-const buttonAnnulla = document.getElementById(btnAnnulla)
+const buttonGenera = document.getElementById("btnGenera");
+const buttonAnnulla = document.getElementById("btnAnnulla");
 
-buttonGener.addEventListener("click", function () {
-    const userkm = document.getElementById(km).value;
-    if (isNaN(usekm)) {
+buttonGenera.addEventListener("click", function () {
+    const userkm = document.getElementById("km").value;
+    if (isNaN(userkm)) {
         alert("i km inseriti non sono validi");
     }
-}
+
+    const userAge=document.getElementById("age").value;
+    if ((userAge.value ==="")) {
+        alert("la fascia d'età inserita non è valida");
+    }
+
+    const userName=document.getElementById("name");
+    console.log(userName.value);
+    fullname.innerHTML = userName;
+
+    const prezzoBase= .21 * userkm;
+
+    if(userAge==="20"){
+        const prezzoFinale = (prezzoBase * .80).toFixed;
+        priceTicket.innerHTML = prezzoFinale;
+        offerta.innerHTML = "20%"
+    }
+})
