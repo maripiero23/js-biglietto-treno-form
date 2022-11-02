151bec7107043b0357f80b079665a0699acf85a7
maripiero23 <mari.pieropan@gmail.com>
2022-11-02 09:41:56 +0100

    modifica1

--- js/main.js
+++ js/main.js
@@ -12,15 +12,29 @@ buttonGenera.addEventListener("click", function () {
         alert("la fascia d'età inserita non è valida");
     }
 
-    const userName=document.getElementById("name");
+    const userName=document.getElementById("name").value;
     console.log(userName.value);
     fullname.innerHTML = userName;
 
-    const prezzoBase= .21 * userkm;
+    const prezzoBase= (.21 * userkm);
 
     if(userAge==="20"){
-        const prezzoFinale = (prezzoBase * .80).toFixed;
+        const prezzoFinale = (prezzoBase * .80);
         priceTicket.innerHTML = prezzoFinale;
         offerta.innerHTML = "20%"
+        console.log("il prezzo finale è", prezzoFinale)
+
+    } else if(userAge==="40"){
+        const prezzoFinale = (prezzoBase * .60);
+        priceTicket.innerHTML = prezzoFinale;
+        offerta.innerHTML = "40%"
+        console.log("il prezzo finale è", prezzoFinale)
+
+    } else{
+        priceTicket.innerHTML = prezzoBase;
+        offerta.innerHTML = "/"
+        console.log("il prezzo finale è", prezzoBase)
+
+
     }
 })
